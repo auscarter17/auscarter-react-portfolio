aee7348f8a24ecf02ebec6dbf8c5519f6b95e056
Austin Carter <98193003+auscarter17@users.noreply.github.com>
2022-06-25 11:42:03 -0400

    fixed nav onClick method

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 
 function Nav(props) {
+  const categories = [
+    {
+      name: "portfolio",
+      description:
+        "Projects I've worked on."
+    },
+    {
+      name: "resume",
+      decription: 
+        "My resume and a list of technologies used."
+    }
+  ]
+
+  function categorySelected(name) {
+    console.log(`${name} selected.`)
+  }
+
   return (
     <header>
       <h2>
@@ -13,6 +30,19 @@ function Nav(props) {
               About me
             </a>
           </li>
+          <li>
+            <span>Contact</span>
+          </li>
+            {categories.map((category) => (
+              <li
+                className="mx-1"
+                key={category.name}
+              >
+                <span onClick={() => categorySelected(category.name)}>
+                  {category.name}
+                </span>
+              </li>
+            ))}
         </ul>
       </nav>
     </header>
